329de8554e800fad4f49d30c2c22a5be5e0a20d4
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-27 17:55:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,17 +5,17 @@ def my_collect(array)
   i = 0
     while (i < array.length)
     lang = array[i]
-    new_array = []
     yield(lang)
     i += 1 
     end
-    
   end
 end
 
 array = ["ruby", "javascript", "python", "objective-c"]
 
 my_collect(array) do |lang|
+  new_array = []
   new_array.push(lang.upcase)
+  new_array
 end
 
